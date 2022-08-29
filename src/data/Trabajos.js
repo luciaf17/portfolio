7199b87b5a542a606ef9dc94219f8c1f86dc42e3
Lucia <luciafutestti@gmail.com>
2022-08-29 18:03:36 -0300

    se corrigen estilos

--- src/data/Trabajos.js
+++ src/data/Trabajos.js
@@ -1,8 +1,8 @@
 export const trabajos = [
     {
-        'id': 'victorroblesweb',
-        'nombre':'Victor Robles Web',
-        'url':'victorroblesweb.com',
+        'id': 'iris',
+        'nombre':'IRIS',
+        'url':'github.com/ibm-grupo-107/Iris-App',
         'tecnologias':'HTML, CSS, JS',
         'categoria':'diseño y desarrollo web',
         'descripcion': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu hendrerit justo, eu accumsan urna. Integer ut nulla vel lacus auctor posuere. Duis eget venenatis augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nisl elit, imperdiet ut mi eget, egestas laoreet elit. Maecenas porttitor sit amet enim ut molestie. Duis sollicitudin quam dolor, in ultricies sapien volutpat elementum. Phasellus faucibus varius libero a mollis. Duis imperdiet odio quis est ultricies iaculis. Nullam vel metus aliquam, auctor magna eu, lobortis magna. Phasellus consequat ipsum eget justo lacinia consectetur. In vehicula ornare magna, vel bibendum nisl dignissim rhoncus. Mauris dapibus dictum tortor non auctor. Vestibulum convallis risus non semper convallis. Praesent eget facilisis augue.'
