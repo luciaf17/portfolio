4b7625f2ee70906141e662c01d8b8aafacc3dda4
Lucia <luciafutestti@gmail.com>
2022-08-30 18:26:55 -0300

    se agregan imagenes

--- src/data/Trabajos.js
+++ src/data/Trabajos.js
@@ -1,11 +1,27 @@
 export const trabajos = [
     {
-        'id': 'cotizador-cripto',
-        'nombre':'Cotizador Cripto',
-        'url':'luciaf17.github.io/cotizador-cripto/',
-        'tecnologias':'HTML, CSS, JS, Bootstrap React JS',
+        'id': 'salesforce',
+        'nombre':'Cloud Consulting Salesforce',
+        'url':'github.com/CloudConsultingProyectLED/CloudConsultingLEDMM',
+        'tecnologias':'Salesforce, Apex, LWC',
         'categoria':'Diseño y Desarrollo Web',
-        'descripcion': 'La app realiza una petición a la api de Coincap, para luego mostrar los precios de las principales criptomonedas. Se realizó un carousel para mostrar las criptos recomendadas. También cuenta con un filtro donde se pueden buscar las criptomonedas que el usuario desee. Repositorio: https://github.com/luciaf17/cotizador-cripto'
+        'descripcion': 'Proyecto para Una consultora que necesitaba una app en Salesforce en la que se pueda llevar un control de los proyectos de cada Project Manager, en la cual se trabajó con LWC, Apex, Flows y Custom Reports'
+    },
+    {
+        'id': 'smart-green',
+        'nombre':'Smart Green App',
+        'url':'github.com/luciaf17/bootcamp-udemy-iot',
+        'tecnologias':'Vue, Node, Express, Mongo, HTML, CSS, Docker, AWS',
+        'categoria':'Diseño y Desarrollo Web',
+        'descripcion': 'APP Fullstack IoT donde se controla automáticamente la temperatura, humedad, nivel de agua, y demás valores mediante una placa ESP32. Se utiliza AWS y Docker.'
+    },
+    {
+        'id': 'iris',
+        'nombre':'Iris App',
+        'url':'github.com/ibm-grupo-107/Iris-App',
+        'tecnologias':'React Native, Figma, Android Studio, Expo, GitHub',
+        'categoria':'Diseño y Desarrollo Mobile - Diseño UX/UI',
+        'descripcion': 'El propósito de la app es que dispongas de una lista con tus ciudades preferidas en la cual puedas acceder fácilmente a los datos del clima en tiempo real. Se consulta a API del clima OpenWeatherMap.'
     },
     {
         'id': 'alimentador',
@@ -16,19 +32,43 @@ export const trabajos = [
         'descripcion': 'Alimentador Automático para Gatos Arduino. Se utiliza un motor de pasos, impresión 3D, ESP32 y pantalla OLED. Aquí se puede ver su funcionamiento: https://www.youtube.com/watch?v=nRCSrGku4ns'
     },
     {
-        'id': 'twitter',
-        'nombre':'Twitter',
-        'url':'twitter.com',
-        'tecnologias':'HTML, CSS, JS',
-        'categoria':'diseño y desarrollo web',
-        'descripcion': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu hendrerit justo, eu accumsan urna. Integer ut nulla vel lacus auctor posuere. Duis eget venenatis augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nisl elit, imperdiet ut mi eget, egestas laoreet elit. Maecenas porttitor sit amet enim ut molestie. Duis sollicitudin quam dolor, in ultricies sapien volutpat elementum. Phasellus faucibus varius libero a mollis. Duis imperdiet odio quis est ultricies iaculis. Nullam vel metus aliquam, auctor magna eu, lobortis magna. Phasellus consequat ipsum eget justo lacinia consectetur. In vehicula ornare magna, vel bibendum nisl dignissim rhoncus. Mauris dapibus dictum tortor non auctor. Vestibulum convallis risus non semper convallis. Praesent eget facilisis augue.'
+        'id': 'dashboard-cripto',
+        'nombre':'Dashboard Análisis Criptomonedas',
+        'url':'public.tableau.com/app/profile/mar.a.luc.a.futestti/viz/AnlisisCriptomonedas-DataBrotes/Dashboard1',
+        'tecnologias':'Tableau',
+        'categoria':'Análisis de Datos - Diseño UX/UI',
+        'descripcion': 'Para la realización del Dashboard, se consultaron los datos de las velas de las criptomonedas Bitcoin, Ethereum y Cardano, en intervalo diario, desde el 16 de julio de 2021 hasta el 16 de julio de 2022. La dirección de la api de Binance donde se realizaron las consultas es la siguiente: https://api.binance.com/api/v3/klines. Luego se especifica símbolo e intervalo. Los datos fueron estructurados en Excel. Se realizó la consulta mediante Power Query y allí se transformaron los datos de formato JSON a una tabla, luego se separaron por ; y se convirtió la fecha que viene en timestamp. Para el mapa, se creó una nueva conexión de datos con otro archivo Excel donde se consultó a la api de Binance nuevamente pero esta vez en tiempo por hora. Luego, se armó un dataset con los países y su correspondiente UTC.'
     },
     {
-        'id': 'linkedin',
-        'nombre':'Linkedin',
-        'url':'linkedin.com',
-        'tecnologias':'HTML, CSS, JS',
-        'categoria':'diseño y desarrollo web',
-        'descripcion': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu hendrerit justo, eu accumsan urna. Integer ut nulla vel lacus auctor posuere. Duis eget venenatis augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nisl elit, imperdiet ut mi eget, egestas laoreet elit. Maecenas porttitor sit amet enim ut molestie. Duis sollicitudin quam dolor, in ultricies sapien volutpat elementum. Phasellus faucibus varius libero a mollis. Duis imperdiet odio quis est ultricies iaculis. Nullam vel metus aliquam, auctor magna eu, lobortis magna. Phasellus consequat ipsum eget justo lacinia consectetur. In vehicula ornare magna, vel bibendum nisl dignissim rhoncus. Mauris dapibus dictum tortor non auctor. Vestibulum convallis risus non semper convallis. Praesent eget facilisis augue.'
+        'id': 'proyecto-automotriz',
+        'nombre':'Dashboard Auto Motors',
+        'url':'public.tableau.com/app/profile/mar.a.luc.a.futestti/viz/Idea6-ProyectoAutomotriz/Dashboard',
+        'tecnologias':'Tableau',
+        'categoria':'Análisis de Datos - Diseño UX/UI',
+        'descripcion': 'Reporte para Challenge #idea6, donde se planteaba un desafío donde dado un dataset se debía realizar un reporte de ventas para una empresa automotriz.'
+    },
+    {
+        'id': 'cotizador-cripto',
+        'nombre':'Cotizador Cripto',
+        'url':'luciaf17.github.io/cotizador-cripto/',
+        'tecnologias':'HTML, CSS, JS, Bootstrap React JS',
+        'categoria':'Diseño y Desarrollo Web',
+        'descripcion': 'La app realiza una petición a la api de Coincap, para luego mostrar los precios de las principales criptomonedas. Se realizó un carousel para mostrar las criptos recomendadas. También cuenta con un filtro donde se pueden buscar las criptomonedas que el usuario desee. Repositorio: https://github.com/luciaf17/cotizador-cripto'
+    },
+    {
+        'id': 'aprograma2',
+        'nombre':'Turismo En La Tierra Media',
+        'url':'github.com/ap-lisajunior/turismo-tierra-media-webapp',
+        'tecnologias':'JAVA, HTML, CSS, Bootstrap, SQL',
+        'categoria':'Diseño y Desarrollo Web',
+        'descripcion': 'Trabajo final Argentina Programa, donde se crea una web fullstack de un parque de atracciones.'
+    },
+    {
+        'id': 'soldagro',
+        'nombre':'Soldagro',
+        'url':'github.com/luciaf17/soldagro-react',
+        'tecnologias':'React JS, Node, Express, SQL, HTML, CSS',
+        'categoria':'Diseño y Desarrollo Web',
+        'descripcion': 'Sistema de control de Stock y Producción para la empresa Soldagro.'
     }
 ];
